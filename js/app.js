faaa7f7208ad50b2fa8c269cfe69a52bff299ad7
Javeria-Saaed-Abbasi <jiyasaeed333@gmail.com>
2020-07-27 03:02:59 +0500

    fix the li error

--- js/app.js
+++ js/app.js
@@ -1,37 +1,42 @@
 var list = document.getElementById('myUL');
 
+
+
 function inputItem(){
+
     var myInupt = document.getElementById('myInput');
     console.log(myInupt.value);
     
     if(myInput.value == "")
-{
-alert("Enter something");
-}
+        {
+        alert("Enter something");
+        }
 
     //create li
     var li = document.createElement('li');
-    var liText = document.createTextNode("myInput.value");
+    li.setAttribute("class","border-bottom border-top li-item border-success text-white mb-3")
+    var liText = document.createTextNode(myInput.value+" ");
     li.appendChild(liText);
 
     //create delete 
     var delBtn = document.createElement("button");
     var delText = document.createTextNode("DELETE");
-    delBtn.setAttribute("class", "btn-success");
-    delBtn.setAttribute("onclick", "delet('this')");
+    delBtn.setAttribute("class", "btn-danger mr-3");
+    delBtn.setAttribute("onclick", "delet(this)");
     delBtn.appendChild(delText);
 
     // create edit btn
     var editbtn = document.createElement("button");
     var editText = document.createTextNode("EDIT");
     editbtn.appendChild(editText)
-    editbtn.setAttribute("class", "btn1")
-    editbtn.setAttribute("onclick","editItem('this')");
+    editbtn.setAttribute("class", "btn-success")
+    editbtn.setAttribute("onclick","editItem(this)");
 
 
 
     li.appendChild(delBtn);
     li.appendChild(editbtn)
+    console.log(li)
     list.appendChild(li);
 
     myInupt.value = "";
@@ -46,6 +51,7 @@ function delet(e){
 
 function deleteAll() {
     list.innerHTML = ""
+
     }
     function editItem(e){
     // e.parentNode.firstChild.nodeValue = "JSA"
@@ -54,5 +60,4 @@ function deleteAll() {
     // console.log(e.parentNode.firstChild.nodeValue)
     e.parentNode.firstChild.nodeValue= editValue
     // console.log(editValue)
-    
     }
